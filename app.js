c9e9f94835b285ac169ed08ec7e6adef463ea851
Thomas Okubo <thomaskokubo@gmail.com>
2022-06-25 10:34:00 -0400

    Temp Update Push

--- app.js
+++ app.js
@@ -4,9 +4,17 @@ function updateData(){
       document.getElementById('lastUpdate').innerHTML = fileDataArray[0];
       document.getElementById('RPIStatus').innerHTML = fileDataArray[1];
       document.getElementById('RPIPollingPeriod').innerHTML = fileDataArray[2];
-      console.log(fileDataArray[2]);
-      console.log(fileDataArray[3]);
       document.getElementById('temperatureOutput').innerHTML = fileDataArray[3];
+
+      if(fileDataArray[1] == "Connected"){
+         document.getElementById('RPIStatus').style.color = 'green';
+      }
+      else if(fileDataArray[1] == "Disconnected"){
+         document.getElementById('RPIStatus').style.color = 'red';
+      }
+      else {
+         document.getElementById('RPIStatus').style.color = 'yellow';
+      }
    });
 }
 
